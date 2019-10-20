a1eaa08b292ef6fb06f305d0f5ae03a00a9e5f4b
Dev Govindji <friendsofdev94@gmail.com>
2019-10-20 17:09:57 -0500

    adds button in appBar to view saved suggestions

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
 
 // #docregion RWS-var
 class RandomWordsState extends State<RandomWords> {
+  void _pushSaved() {}
   final _suggestions = <WordPair>[];
   final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
@@ -71,6 +72,9 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
