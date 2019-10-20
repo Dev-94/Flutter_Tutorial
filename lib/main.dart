ee6eb45e03ecf7244957999ea46fd61fa46d9364
Dev Govindji <friendsofdev94@gmail.com>
2019-10-20 17:25:33 -0500

    adds route to 'saved suggestions' page populated with users favorites

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,36 @@ class MyApp extends StatelessWidget {
 
 // #docregion RWS-var
 class RandomWordsState extends State<RandomWords> {
-  void _pushSaved() {}
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
   final _suggestions = <WordPair>[];
   final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
